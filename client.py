de97bb99a5f385cc344922e03c480096f5e70804
daraahh <pikachu.dario@gmail.com>
2019-12-05 00:32:00 +0100

    Arreglo funcion auth

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ import os
 
 def authenticate():
     auth = tweepy.OAuthHandler(os.environ['API_KEY'], os.environ['API_SECRET_KEY'])
-    auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN'])
+    auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
     api = tweepy.API(auth)
     return api
 
